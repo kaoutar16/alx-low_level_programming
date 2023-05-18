4a73efd65e61847d1606ea0238a1953be74737c3
kaoutar16 <farnane.kaoutar@gmail.com>
2023-05-18 03:43:49 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,8 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define ERR_MSG "Error"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * is_digit - checks if a string contains a non-digit char
